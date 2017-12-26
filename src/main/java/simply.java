49629d607e919883b26d82a47ae1c10f1ab82df6
xiaodiaoyuo <854150407@qq.com>
2017-12-26 23:56:51 +0800

    first add

---
+++ src/main/java/simply.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by Administrator on 2017/12/26 0026.
+ */
+public class simply {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        System.out.println("----1---------");
+    }
+
+}
