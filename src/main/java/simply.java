bbf3e371a92e339e3f1496c39a7eb511f5f4ec3e
xiaodiaoyuo <854150407@qq.com>
2017-12-27 00:13:01 +0800

    hashmap add

--- src/main/java/simply.java
+++ src/main/java/simply.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -7,6 +8,7 @@ import java.util.concurrent.TimeoutException;
 public class simply {
     public static void main(String[] args) throws IOException, TimeoutException {
         System.out.println("----1---------");
+        Map<String,String> map=new HashMap<>();
     }
 
 }
